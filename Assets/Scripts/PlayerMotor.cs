aeafe32fc76b4113b90c8ac8ddccd3a0a531ba7c
Adrian Nicolae <adrian.danut.nicolae@gmail.com>
2020-03-12 16:15:15 +0200

    Adding Initial Project Files

---
+++ Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class PlayerMotor : MonoBehaviour
+{
+
+    private CharacterController controller;
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        controller.Move(Vector3.forward * 4.0f * Time.deltaTime);
+    }
+}
