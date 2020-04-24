dc6498f29f4ef3fda54d77ab31626d04709856a8
AvramAlexandraIoana <37377341+AvramAlexandraIoana@users.noreply.github.com>
2020-04-24 15:20:35 +0100

    Set position column

--- Assets/Scripts/HighScoreTable.cs
+++ Assets/Scripts/HighScoreTable.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class HighScoreTable : MonoBehaviour
 {
     private Transform entryContainer;
@@ -21,6 +21,29 @@ public class HighScoreTable : MonoBehaviour
             RectTransform entryRectTransform = entryTransform.GetComponent<RectTransform>();
             entryRectTransform.anchoredPosition = new Vector2(0, -templateHeight * i);
             entryTransform.gameObject.SetActive(true);
+
+           int rank = i + 1;
+            string rankString;
+            switch (rank)
+            {
+                default:
+                    rankString = rank + "TH"; break;
+                case 1:
+                    rankString = "1ST"; break;
+                case 2:
+                    rankString = "2ND"; break;
+                case 3:
+                    rankString = "3RD"; break;
+            }
+
+            entryTransform.Find("posText").GetComponent<Text>().text = rankString;
+
+            int score = Random.Range(0, 10000);
+            entryTransform.Find("scoreText").GetComponent<Text>().text = score.ToString();
+
+            string name = "AAA";
+            entryTransform.Find("nameText").GetComponent<Text>().text = name;
+
 
         }
 
